f430d4548e261d406dc521bb0cffdfae8131f741
ezeeyahoo <guptaeshant@gmail.com>
2020-04-28 21:21:11 +0530

    Detecting Run state, does not exit prematurely.

--- LaunchTrackpad.sh
+++ LaunchTrackpad.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 exec /System/Applications/System\ Preferences.app/Contents/MacOS/System\ Preferences /System/Library/PreferencePanes/Trackpad.prefPane &
 pid=$!
-sleep 8
-kill $pid
+
+# state=$(ps aux | grep Trackpad.prefPane | grep -v grep | awk '{print $8}'
+sleep 2
+
+while [ "$state" != "S+" ]
+do
+  sleep 1
+  state=$(ps aux | grep Trackpad.prefPane | grep -v grep | awk '{print $8}')
+done
+exec kill $pid
+exit 0

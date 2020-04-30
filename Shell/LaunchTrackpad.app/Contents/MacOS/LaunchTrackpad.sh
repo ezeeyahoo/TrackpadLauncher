cbb1c46aced34018c384e9f840d8ad6c261ecb08
ezeeyahoo <guptaeshant@gmail.com>
2020-04-30 11:33:21 +0530

    standalone script run work flawlessly

--- Shell/LaunchTrackpad.app/Contents/MacOS/LaunchTrackpad.sh
+++ Shell/LaunchTrackpad.app/Contents/MacOS/LaunchTrackpad.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-exec /System/Applications/System\ Preferences.app/Contents/MacOS/System\ Preferences /System/Library/PreferencePanes/Trackpad.prefPane &
+/System/Applications/System\ Preferences.app/Contents/MacOS/System\ Preferences /System/Library/PreferencePanes/Trackpad.prefPane &
 pid=$!
-
 sleep 2
-while [ "$state" != "S" ] || [ "$state" != "S+" ]
+declare state
+while [ "$state" != "S" ] && [ "$state" != "S+" ]
 do
   state=$(ps aux | grep Trackpad.prefPane | grep -v grep | awk '{print $8}')
+  if -z $state
+  then
+    exit 1
+  fi
 done
-exec kill $pid
+kill $pid
 exit 0

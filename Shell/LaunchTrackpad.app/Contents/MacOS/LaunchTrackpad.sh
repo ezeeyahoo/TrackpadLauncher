0763a5168a3b2accdedb7f14f9c632cc2a134d13
Eshant Gupta <eshant@IshuMacBookPro.local>
2020-05-03 17:14:40 +0530

    Error handling

--- Shell/LaunchTrackpad.app/Contents/MacOS/LaunchTrackpad.sh
+++ Shell/LaunchTrackpad.app/Contents/MacOS/LaunchTrackpad.sh
@@ -2,14 +2,24 @@
 /System/Applications/System\ Preferences.app/Contents/MacOS/System\ Preferences /System/Library/PreferencePanes/Trackpad.prefPane &
 pid=$!
 sleep 2
+
 declare state
 while [ "$state" != "S" ] && [ "$state" != "S+" ]
 do
   state=$(ps aux | grep Trackpad.prefPane | grep -v grep | awk '{print $8}')
-  if -z $state
+
+  if [ -z $state ]
   then
+    kill $pid
+    logger -s "Terminated ABNORMALLY!!!" >> ~/.trackpad.log
     exit 1
   fi
 done
+
 kill $pid
+if [ $? -ne 0 ]
+then
+  logger -s "Terminated ABNORMALLY!!!" >> ~/.trackpad.log
+  exit 1
+fi
 exit 0
